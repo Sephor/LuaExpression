41c97e36280682b60820071acde4cc6ea6838581
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2015-11-12 01:28:23 +0100

    added most missing expressions

--- expressions.py
+++ expressions.py
@@ -1,27 +1,32 @@
-
 import re
 
 class AbstractExpression(object):
 
 	def __init__(self, string):
-		self.raw = string
+		self.raw = string.strip()
+
+	@classmethod
+	def accepts(cls, string):
+		exp = cls(string)
+		return exp.is_valid()
 
 	def is_valid(self):
 		return False
 
 	def evaluate(self):
-		if self.is_valid:
+		if self.is_valid():
 			return self.evaluate_valid()
 
 	def evaluate_valid(self):
 		return None
+
 
 #http://www.lua.org/manual/5.3/manual.html#3.4
 class BasicExpression(AbstractExpression):
 	re_validator = re.compile(r'(false)|(true)|(nil)')
 
 	def is_valid(self):
-		return self.re_validator.fullmatch(self.raw)
+		return self.re_validator.fullmatch(self.raw) != None
 
 	def evaluate_valid(self):
 		if self.raw == 'nil':
@@ -58,20 +63,220 @@ class NumeralExpression(AbstractExpression):
 
 
 class LiteralStringExpression(AbstractExpression):
+	#matches a string in the long bracket form
 	re_long_bracket = re.compile(
 		r'\[(?P<eq>=*)\[(?P<content>.*?[^\\])\](?P=eq)\]', re.S)
+	#matches a string with single or double quotes
 	re_quotes = re.compile(r"""(?P<qt>'|")(?P<content>.*?)(?<=[^\\])(?P=qt)"""
 		, re.S)
+	escape_map = {'n': '\n', 'r': '\r', 't': '\t', 'v': '\v'
+		, '\\': '\\', '\"': '\"', '\'': '\''}
+	#matches all lua escape sequences except \u{XXX}
+	re_escape_sequence = re.compile(r'\\(?:(?P<dec>\d{1,3})|'
+		+ '(?P<hex>x[0-9a-fA-F]{2})|(?P<char>[nrtv\\\"\']))')
 
 	def has_quotes(self):
-		print(self.re_quotes.fullmatch(self.raw))
-		return self.re_quotes.fullmatch(self.raw)
+		match = self.re_quotes.match(self.raw)
+		if match and match.end() == len(self.raw):
+			self.content = match.group('content')
+			return True
+		return False
 
 	def has_long_brackets(self):
-		return self.re_long_bracket.fullmatch(self.raw)
+		match = self.re_long_bracket.match(self.raw)
+		if match and match.end() == len(self.raw):
+			self.content = match.group('content')
+			return True
+		return False
 
 	def is_valid(self):
-		return (self.has_quotes() or self.has_long_brackets()) != None
+		return self.has_quotes() or self.has_long_brackets()
+
+	def find_content_and_type(self):
+		self.has_quotes()
+		is_raw = self.has_long_brackets()
+		return (self.content, is_raw)
+
+	def interpret_raw(self, content):
+		if len(content) > 0 and content[0] == '\n':
+			return content[1:]
+		return content
+
+	def escape_sequence(self, match):
+		groups = match.groupdict()
+		if groups['dec']:
+			return chr(int(match.group('dec')))
+		if groups['hex']:
+			hex_number = match.group('hex')[1:]
+			return chr(int(hex_number))
+		if groups['char']:
+			character = match.group('char')
+			return self.escape_map[character]
+
+	def interpret(self):
+		temp = self.re_escape_sequence.sub(self.escape_sequence, self.content)
+		return temp
 
 	def evaluate_valid(self):
-		pass
+		content, is_raw = self.find_content_and_type()
+		if is_raw:
+			return self.interpret_raw(content)
+		return self.interpret()
+
+class TableConstructorExpression(AbstractExpression):
+	field_seperators = [',', ';']
+	re_name = re.compile(r'(?P<key>[a-zA-Z_]\w*)\s*=')
+
+	def extract_bracketed_expression(self, string):
+		string = string[1:]
+		exp_candidates = string.split(']')
+		exp_string = exp_candidates.pop(0)
+		while True:
+			exp = GenericExpression(exp_string)
+			if exp.is_valid():
+				self.keys_expressions.append(exp)
+				break
+			if len(exp_candidates) <= 0:
+				return False
+			exp_string += ']' + exp_candidates.pop(0)
+		string = string[len(exp_string) + 1:]
+		string = string.strip()
+		if string[0] != '=':
+			return False
+		exp = GenericExpression(string[1:])
+		if exp.is_valid():
+			self.value_expressions.append(exp)
+			return True
+		return False
+
+
+	def extract_named_value(self, match):
+		key_exp = GenericExpression('"' + match.group('key') + '"')
+		value_string = match.string[match.end():]
+		value_exp = GenericExpression(value_string)
+		if value_exp.is_valid():
+			self.keys_expressions.append(key_exp)
+			self.value_expressions.append(value_exp)
+			return True
+		return False
+
+	def extract_value(self, string):
+		key_exp = GenericExpression(str(self.default_key))
+		self.default_key += 1
+		value_exp = GenericExpression(string)
+		if value_exp.is_valid():
+			self.keys_expressions.append(key_exp)
+			self.value_expressions.append(value_exp)
+			return True
+		return False
+
+	def is_field(self, string):
+		string = string.strip()
+		#‘[’ exp ‘]’ ‘=’ exp
+		if string[0] == '[':
+			return self.extract_bracketed_expression(string)
+		#Name ‘=’ exp
+		match = self.re_name.match(string)
+		if match:
+			return self.extract_named_value(match)
+		return self.extract_value(string)
+
+	def get_field_candidates(self, string):
+		field_candidates = []
+		seperators = []
+		start = 0
+		for i, char in enumerate(string):
+			for sep in self.field_seperators:
+				if char == sep:
+					seperators.append(sep)
+					field_candidates.append(string[start:i])
+					start = i + 1
+		if start < len(string):
+			field_candidates.append(string[start:])
+		return (field_candidates, seperators)
+
+	def is_field_list(self, string):
+		self.fields = []
+		self.keys_expressions = []
+		self.default_key = 1
+		self.value_expressions = []
+		field_candidates, seperators = self.get_field_candidates(string)
+		while len(field_candidates) > 0:
+			candidate = field_candidates.pop(0)
+			while not self.is_field(candidate):
+				if len(field_candidates) <= 0:
+					return False
+				candidate += seperators.pop(0) + field_candidates.pop(0)
+			self.fields.append(candidate)
+			if len(seperators) > 0:
+				seperators.pop(0)
+		if len(seperators) <= 1:
+			return True
+		return False
+
+	def is_valid(self):
+		if len(self.raw) >= 2 and self.raw[0] == '{' and self.raw[-1] == '}':
+			return self.is_field_list(self.raw[1:-1])
+		return False
+
+	def evaluate_valid(self):
+		if len(self.keys_expressions) != len(self.value_expressions):
+			raise Exception('there are a different ammount of keys and values.'
+				+ 'This should never happen.')
+		result = []
+		for key, value in zip(self.keys_expressions, self.value_expressions):
+			result.append((key.evaluate(), value.evaluate()))
+		return result
+
+class PrefixExpression(AbstractExpression):
+	#only supports prefixexp ::= ‘(’ exp ‘)’
+
+	def is_valid(self):
+		if len(self.raw) <= 2:
+			return False
+		self.internal_exp = GenericExpression(self.raw[1:-1])
+		return (self.raw[0] == '(' and self.raw[-1] == ')'
+			and self.internal_exp.is_valid())
+
+	def evaluate_valid(self):
+		return self.internal_exp.evaluate()
+
+
+class UnaryOperatorExpression(AbstractExpression):
+	#only supports unary operators ‘-’ and ‘not’
+	re_unop = re.compile(r'-|(not)')
+
+	def is_valid(self):
+		match = self.re_unop.match(self.raw)
+		if match:
+			self.unop = match.group(0)
+			if self.unop == '-':
+				self.expression = NumeralExpression(self.raw[1:])
+				return self.expression.is_valid()
+			if self.unop == 'not':
+				self.expression = BasicExpression(self.raw[3:])
+				return self.expression.is_valid()
+		return False
+
+	def evaluate_valid(self):
+		if self.unop == '-':
+			return - self.expression.evaluate()
+		if self.unop == 'not':
+			return not self.expression.evaluate()
+
+
+class GenericExpression(AbstractExpression):
+	known_expressions = [BasicExpression, NumeralExpression,
+		LiteralStringExpression, TableConstructorExpression]
+
+	def is_valid(self):
+		for expression_type in self.known_expressions:
+			exp = expression_type(self.raw)
+			if exp.is_valid():
+				self.expression = exp
+				return True
+		return False
+
+	def evaluate_valid(self):
+		#self.expression.is_valid has already been checked, so this is fine
+		return self.expression.evaluate_valid()
--- test.py
+++ test.py
@@ -4,11 +4,15 @@ import expressions
 
 class MyTestCase(unittest.TestCase):
 
-	def assertYields(self, function, input_values, expected_outputs):
+	def assertYields(self, function, input_values, expected_outputs, msg=None):
 		for i in range(len(input_values)):
-			with self.subTest(i=i):
-				self.assertEqual(function(*input_values[i]),
-					expected_outputs[i])
+			result = function(*input_values[i])
+			if result != expected_outputs[i]:
+				msg = self._formatMessage(msg,
+					'at index {}: {} of {} did not yield "{}" but "{}"'.format(
+						i, function.__name__, input_values[i],
+						expected_outputs[i], result))
+				raise self.failureException(msg)
 
 
 class TestBasicExpressions(unittest.TestCase):
@@ -45,7 +49,7 @@ class TestNumeralExpression(MyTestCase):
 	def test_is_valid(self):
 		valid = ['3', '345', '0xff', '0xBEBADA', '3.0', '3.1416', '314.16e-2',
 			'0.31416E1', '34e1', '.1']
-		invalid = ['', '3 ', '11.45.2', 'abf']
+		invalid = ['', '3_', '11.45.2', 'abf']
 		self.assertYields(self.is_valid, valid, [True] * len(valid))
 		self.assertYields(self.is_valid, invalid, [False] * len(invalid))
 
@@ -66,7 +70,6 @@ class TestLiteralStringExpression(MyTestCase):
 
 	def is_valid(self, *s):
 		s = ''.join(s)
-		print(s)
 		exp = expressions.LiteralStringExpression(s)
 		return exp.is_valid()
 
@@ -87,3 +90,99 @@ class TestLiteralStringExpression(MyTestCase):
 			r"""'\97lo\10\04923"'""", '[[alo\n123"]]', '[==[\nalo\n123"]==]']
 		expected_outputs = ['', '',] + ['alo\n123"'] * 5
 		self.assertYields(self.evaluate_valid, input_values, expected_outputs)
+
+class TestGenericExpression(MyTestCase):
+
+	def is_valid(self, *s):
+		s = ''.join(s)
+		exp = expressions.GenericExpression(s)
+		return exp.is_valid()
+
+	def test_is_valid(self):
+		valid = ['"foo"', '234', 'false']
+		invalid = ['', '1.1.1', 'abc']
+		self.assertYields(self.is_valid, valid, [True] * len(valid))
+		self.assertYields(self.is_valid, invalid, [False] * len(invalid))
+
+	def evaluate(self, *s):
+		s = ''.join(s)
+		exp = expressions.GenericExpression(s)
+		return exp.evaluate()
+
+	def test_evaluate(self):
+		input_values = ['"foo"', '234', 'false']
+		expected_outputs = ['foo', 234, False]
+		self.assertYields(self.evaluate, input_values, expected_outputs)
+
+
+class TestTableConstructorExpression(MyTestCase):
+
+	def is_valid(self, *s):
+		s = ''.join(s)
+		exp = expressions.TableConstructorExpression(s)
+		return exp.is_valid()
+
+	def test_is_valid(self):
+		valid = ['{}', '{1,2,3}', '{ 1\t;2,\n3}', "{['a'] = 1.1}", '{{}}']
+		invalid = ['{{}', '{1 1 2}', '[]']
+		self.assertYields(self.is_valid, valid, [True] * len(valid))
+		self.assertYields(self.is_valid, invalid, [False] * len(invalid))
+
+	def evaluate(self, *s):
+		s = ''.join(s)
+		exp = expressions.TableConstructorExpression(s)
+		return exp.evaluate()
+
+	def test_evaluate(self):
+		input_values = ['{}', '{1,2,3}', '{ 1\t;2,\n3}', "{['a'] = 1.1}"
+			, '{{}}']
+		expected_outputs = [[], [(1, 1), (2, 2), (3, 3)]
+			, [(1, 1), (2, 2), (3, 3)], [('a', 1.1)], [(1, [])]]
+		self.assertYields(self.evaluate, input_values, expected_outputs)
+
+
+class TestPrefixExpression(MyTestCase):
+
+	def is_valid(self, *s):
+		s = ''.join(s)
+		exp = expressions.PrefixExpression(s)
+		return exp.is_valid()
+
+	def test_is_valid(self):
+		valid = ['("hello")', '(123)', '(nil)']
+		invalid = ['()', '((123)', '']
+		self.assertYields(self.is_valid, valid, [True] * len(valid))
+		self.assertYields(self.is_valid, invalid, [False] * len(invalid))
+
+	def evaluate(self, *s):
+		s = ''.join(s)
+		exp = expressions.PrefixExpression(s)
+		return exp.evaluate()
+
+	def test_evaluate(self):
+		input_values = ['("hello")', '(123)', '(nil)']
+		expected_outputs = ['hello', 123, None]
+		self.assertYields(self.evaluate, input_values, expected_outputs)
+
+class TestUnaryOperatorExpression(MyTestCase):
+
+	def is_valid(self, *s):
+		s = ''.join(s)
+		exp = expressions.UnaryOperatorExpression(s)
+		return exp.is_valid()
+
+	def test_is_valid(self):
+		valid = ['-1', '-1.234', 'not true', 'not nil']
+		invalid = ['-"hello"', '-nil', '123']
+		self.assertYields(self.is_valid, valid, [True] * len(valid))
+		self.assertYields(self.is_valid, invalid, [False] * len(invalid))
+
+	def evaluate(self, *s):
+		s = ''.join(s)
+		exp = expressions.UnaryOperatorExpression(s)
+		return exp.evaluate()
+
+	def test_evaluate(self):
+		input_values = ['-1', '-1.234', 'not true', 'not nil']
+		expected_outputs = [-1, -1.234, False, True]
+		self.assertYields(self.evaluate, input_values, expected_outputs)
